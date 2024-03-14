2f30317e06e850e6b73e2a92f35a35d733f0b6a2
Michaelle Valenzuela <michaelle.valenzuela@epn.edu.ec>
2024-03-14 09:26:21 -0500

    Solucion error Modificar perfil

--- src/CORE/Controller/User.controller.ts
+++ src/CORE/Controller/User.controller.ts
@@ -43,23 +43,39 @@ export const editProfileUser = async (req: any, res: any) => {
             const { email, password, confirm_password, } = req.body;
             let arr_err: string[] = [];
 
-            for (let c in req.body) {
-                if (req.body[c].trim() === "") arr_err.push(`El campo ${c} es requerido`);
-            }
+            // for (let c in req.body) {
+            //     if (req.body[c].trim() === "") arr_err.push(`El campo ${c} es requerido`);
+            // }
 
             const REGEX_PASS = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,24}$/;
-
-            if (password !== confirm_password) arr_err.push(`Las contraseñas ingresadas deben ser iguales`);
-            if (!REGEX_PASS.test(password)) arr_err.push("La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, una mayúscula y un símbolo")
-
-            if (arr_err.length > 0) res.status(404).json({ ok: false, errors: arr_err })
-            else {
-                bcrypt.hash(password, 10, async function (err, hash) {
-                    User.findOneAndUpdate({ _id: req.params.id }, {
-                        password: hash, email: email.toLowerCase()
-                    }).exec().then((success: any) => res.status(201).json({ ok: true, msg: "La información ha sido actualizada exitosamente" }));
-                });
+            if(email != "" && password === "" && confirm_password === ""){
+                
+                if (arr_err.length > 0) res.status(404).json({ ok: false, errors: arr_err })
+                else {
+                    bcrypt.hash(password, 10, async function (err, hash) {
+                        User.findOneAndUpdate({ _id: req.params.id }, {
+                            //password: hash, 
+                            email: email.toLowerCase()
+                        }).exec().then((success: any) => res.status(201).json({ ok: true, msg: "La información ha sido actualizada exitosamente" }));
+                    });
+                }
             }
+            //En caso de que se genere problema de la linea 52 a 61 van fuera del If hay q descomentar el for
+            //
+            else if(email != ""){
+                if (password !== confirm_password) arr_err.push(`Las contraseñas ingresadas deben ser iguales`);
+                if (!REGEX_PASS.test(password)) arr_err.push("La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, una mayúscula y un símbolo")
+                if (arr_err.length > 0) res.status(404).json({ ok: false, errors: arr_err })
+                else {
+                    bcrypt.hash(password, 10, async function (err, hash) {
+                        User.findOneAndUpdate({ _id: req.params.id }, {
+                            password: hash, 
+                            email: email.toLowerCase()
+                        }).exec().then((success: any) => res.status(201).json({ ok: true, msg: "La información ha sido actualizada exitosamente" }));
+                    });
+                }
+            }
+            
         }
     });
 }
